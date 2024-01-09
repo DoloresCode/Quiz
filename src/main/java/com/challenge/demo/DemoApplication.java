0cad3c1dc907ea2b5d579efd0da4dcb66e544213
Dolores Crazover <doloresnetweb@gmail.com>
2024-01-09 16:47:41 -0500

    PostgreSQL database created

--- src/main/java/com/challenge/demo/DemoApplication.java
+++ src/main/java/com/challenge/demo/DemoApplication.java
@@ -2,14 +2,15 @@ package com.challenge.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@PropertySource("classpath:env.properties") 
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
