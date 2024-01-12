511932e40c804363a249060fa225e12ab2a4dbd2
Dolores Crazover <doloresnetweb@gmail.com>
2024-01-12 18:49:05 -0500

    test of the API in progress, MatrixQuestionController creation of row and colums dynamics in progress see comments added to follow

--- src/main/java/com/challenge/demo/controllers/MatrixQuestionController.java
+++ src/main/java/com/challenge/demo/controllers/MatrixQuestionController.java
@@ -36,17 +36,37 @@ public class MatrixQuestionController {
     @Autowired
     private UserService userService;
 
+
     @PostMapping
     public ResponseEntity<MatrixQuestion> createOrUpdateMatrixQuestion(@RequestBody MatrixQuestion matrixQuestion) {
         MatrixQuestion savedMatrixQuestion = matrixQuestionService.saveOrUpdateMatrixQuestion(matrixQuestion);
         return new ResponseEntity<>(savedMatrixQuestion, HttpStatus.CREATED);
     }
 
+    //Create a POST Mapping method to handle Row creation. Use URL like /matrix-questions/{matrixQuestionID}/rows 
+
+    //Create a POST Mapping method to handle Columns creation. Use URL like /matrix-questions/{matrixQuestionID}/columns
+
+    //Create a GET Mapping method to handle Row creation.  Use URL like /matrix-questions/{matrixQuestionID}/rows
+
+    //Create a GET Mapping method to handle Columns creation. Use URL like /matrix-questions/{matrixQuestionID}/rows
+    
+    //Create the PUT Mapping method to handle Row update. Use URL like /matrix-questions/{matrixQuestionID}/rows/{rowID}
+
+    //Create the PUT Mapping method to handle Column update. Use URL like /matrix-questions/{matrixQuestionID}/columns/{columnID}
+    
+    // Create the Delete method to handle Row deletion. Use URL like /matrix-questions/{matrixQuestionID}/rows/{rowID}
+
+    // Create the Delete method to handle Column deletion. Use URL like /matrix-questions/{matrixQuestionID}/columns/{columnID}
+
+    // After test with Postman and see that everything is created in the database H2
+
+    // Create the README.md file and add the instructions to run the project
+
     @GetMapping("/{userUuid}/{siteUuid}")
-    public ResponseEntity<MatrixQuestion> getQuestionForUser(@PathVariable UUID userUuid) {
+    public ResponseEntity<MatrixQuestion> getQuestionForUser(@PathVariable UUID userUuid,@PathVariable UUID siteUuid) {
         User user = userService.createUserWithUUID(userUuid);
         MatrixQuestion question = matrixQuestionService.getNextQuestionForUser(user);
         return new ResponseEntity<>(question, HttpStatus.OK);
     }
-
 }
